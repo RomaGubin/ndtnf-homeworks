b2c56f9ebeae88385ba696c9c9f8bacb173dc7ff
Roman G <roma.gubin2011@yandex.ru>
2024-10-07 21:11:55 +0500

    Initial commit1

--- 009-rxjs/src/rxjs/rxjs.controller.ts
+++ 009-rxjs/src/rxjs/rxjs.controller.ts
@@ -7,7 +7,7 @@ export class RxjsController {
   constructor(private rxjsService: RxjsService) {}
 
   @Get("repositories/")
-  async repositories(@Query() { text, hub }: IParamText) {
-    return await this.rxjsService.searchRepositories(text, hub);
+  async repositories(@Query() { text }: IParamText) {
+    return await this.rxjsService.searchBothPlatforms(text);
   }
 }
